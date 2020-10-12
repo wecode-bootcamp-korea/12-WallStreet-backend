eb159c1d0700530e2ea7ddfad26109778d29e413
Soojeong Kim <ksooj.kim@gmail.com>
2020-10-12 22:19:21 +0900

    [ADD] Social Log In

--- user/tests.py
+++ user/tests.py
@@ -2,8 +2,11 @@ import json
 import bcrypt
 import jwt
 
-from django.test import TestCase, Client
-from .models     import User
+from django.test    import TestCase, Client
+from unittest.mock  import patch, MagicMock
+
+from wallstreet.settings    import SECRET_KEY, ALGORITHM
+from .models                import User
 
 class SignUpTest(TestCase):
     def setUp(self):
@@ -59,3 +62,79 @@ class SignUpTest(TestCase):
         response = client.post('/account/signup', json.dumps(user), content_type = 'application/json')
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {'message':'INVALID_INFOS'})
+
+class SosialSignInTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            name          = '테스트일',
+            password      = 'test_password',
+            email         = '테스트일12345',
+           )
+
+    def tearDown(self):
+        User.objects.all().delete()
+     
+    @patch("user.views.requests")
+    def test_social_signup_success(self, mocked_requests):
+        client = Client()
+        class MockedResponseTwo:
+            def json(self):
+                return {"id" : "98765", 
+                        'kakao_account': {  "profile" : {"nickname" : "테스트이"},   }
+                        }
+
+        mocked_requests.get = MagicMock(return_value = MockedResponseTwo())
+        response = client.post("/user/socialsignin", **{"Authorization":"1234","content_type" : "application/json"})
+
+        accessed_user = User.objects.get(email='테스트이98765')
+        access_token = jwt.encode({'user_id': accessed_user.id}, SECRET_KEY, ALGORITHM)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'Authorization': access_token.decode('utf-8')})
+
+    @patch("user.views.requests")
+    def test_social_signin_success(self, mocked_requests):
+        client = Client()
+        class MockedResponseOne:
+            def json(self):
+                return {"id" : "12345", 
+                        'kakao_account': {  "profile" : {"nickname" : "테스트일"},   }
+                        }
+
+        mocked_requests.get = MagicMock(return_value = MockedResponseOne())
+        response = client.post("/user/socialsignin", **{"Authorization":"1234","content_type" : "application/json"})
+
+        accessed_user = User.objects.get(email='테스트일12345')
+        access_token = jwt.encode({'user_id': accessed_user.id}, SECRET_KEY, ALGORITHM)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'Authorization': access_token.decode('utf-8')})
+        
+    @patch("user.views.requests")
+    def test_social_signup_invalid_user_from_kakao(self, mocked_requests):
+        client = Client()
+        class MockedResponseOne:
+            def json(self):
+                return {'msg':'INVALID_TOKEN'}
+
+        mocked_requests.get = MagicMock(return_value = MockedResponseOne())
+
+        response = client.post("/user/socialsignin", **{"Authorization":"1234","content_type" : "application/json"})
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {'message': 'KEY_ERROR'})
+        
+    @patch("user.views.requests")
+    def test_social_signup_no_key_from_front_key_error(self, mocked_requests):
+        client = Client()
+        class MockedResponseOne:
+            def json(self):
+                return {'msg':'KEY_ERROR'}
+ 
+        mocked_requests.get = MagicMock(return_value = MockedResponseOne())
+ 
+        response = client.post("/user/socialsignin", **{"authooooo":"1234","content_type" : "application/json"})
+ 
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {'message': 'KEY_ERROR'})
+
